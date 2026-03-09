cb3b6075454ef2c0fae0f82daf220b7562013ebe
Karl Seguin <k@openmymind.io>
2026-03-09 13:14:57 +0800

    Use Session as a container for cross-frame resources
    The introduction of frames means that data is no longer tied to a specific Page
    or Context. 255b9a91cc409adc685806878b716dc834fb991a introduced Origins for
    v8 values shared across frames of the same origin. The commit highlighted the
    lifetime mismatched that we now have with data that can outlive 1 frame. A
    specific issue with that commit was the finalizers were still Context-owned.
    But like any other piece of data, that isn't right; aside from modules, nothing
    should be context-owned.
    
    This commit continues where the last left off and moves finalizers from Context
    to Origin. This is done in a separate commit because it introduces significant
    changes. Currently, finalizers take a *Page, but that's no longer correct. A
    value created in one Page, can outlive the Page. We need another container. I
    original thought to use Origin, but that isn't know to CDP/MCP. Instead, I
    decide to enhance the Session.
    
    Session is now the owner of the page.arena, the page.factory and the
    page.arena_pool. Finalizers are given a *Session which they can use to release
    their arena.

--- src/browser/webapi/net/Response.zig
+++ src/browser/webapi/net/Response.zig
@@ -21,6 +21,7 @@ const js = @import("../../js/js.zig");
 const Http = @import("../../../http/Http.zig");
 
 const Page = @import("../../Page.zig");
+const Session = @import("../../Session.zig");
 const Headers = @import("Headers.zig");
 const ReadableStream = @import("../streams/ReadableStream.zig");
 
@@ -76,7 +77,7 @@ pub fn init(body_: ?[]const u8, opts_: ?InitOpts, page: *Page) !*Response {
     return self;
 }
 
-pub fn deinit(self: *Response, shutdown: bool, page: *Page) void {
+pub fn deinit(self: *Response, shutdown: bool, session: *Session) void {
     if (self._transfer) |transfer| {
         if (shutdown) {
             transfer.terminate();
@@ -85,7 +86,7 @@ pub fn deinit(self: *Response, shutdown: bool, page: *Page) void {
         }
         self._transfer = null;
     }
-    page.releaseArena(self._arena);
+    session.releaseArena(self._arena);
 }
 
 pub fn getStatus(self: *const Response) u16 {
